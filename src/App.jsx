d9fd7e620edc7ff787218469d425cb9174fcff91
Ojas-engg <ladojaswini@gmail.com>
2024-10-03 16:11:57 +0530

    Navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Components/component/navbar'
+import Navbar from './Components/component/Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
